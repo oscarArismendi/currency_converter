deacb48618a7ea6c74b65aa13be969310d7742a2
oscarArismendi <oscarfernandoarismendigalvis@gmail.com>
2024-05-19 17:51:47 -0500

    feat: first deploy

---
+++ lib/pages/currency_converter_material_page.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class CurrencyConverterMaterialPage extends StatelessWidget{
+  const CurrencyConverterMaterialPage({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.blueGrey,
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children:[
+              const Text("0",
+              style: TextStyle(
+                fontSize:45,
+                fontWeight: FontWeight.bold,
+                color: Color.fromARGB(255, 255, 255, 255),
+              )
+              ),
+              TextField(
+                style: const TextStyle(
+                  color: Colors.black,
+                ),
+                decoration: InputDecoration(
+                  hintText: "Please enter the amount in USD",
+                  hintStyle: const TextStyle(
+                    color: Colors.black,
+                  ),
+                  prefixIcon: const Icon(Icons.monetization_on_outlined),
+                  prefixIconColor: Colors.black,
+                  filled:true,
+                  fillColor: Colors.white,
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: const BorderSide(
+                      width: 2.0,
+                      style: BorderStyle.solid,
+                    ),
+                    borderRadius: BorderRadius.circular(40),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      );
+  }
+}
