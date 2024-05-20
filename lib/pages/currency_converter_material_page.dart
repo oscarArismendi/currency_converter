51f2f61e0342adacdfc95f4b02ac7249de059d02
oscarArismendi <oscarfernandoarismendigalvis@gmail.com>
2024-05-19 22:27:54 -0500

    feat:end convert button

--- lib/pages/currency_converter_material_page.dart
+++ lib/pages/currency_converter_material_page.dart
@@ -1,9 +1,17 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class CurrencyConverterMaterialPage extends StatelessWidget{
   const CurrencyConverterMaterialPage({super.key});
   @override
   Widget build(BuildContext context) {
+    final border = OutlineInputBorder(
+                    borderSide: const BorderSide(
+                      width: 2.0,
+                      style: BorderStyle.solid,
+                    ),
+                    borderRadius: BorderRadius.circular(40),
+                  );
     return Scaffold(
         backgroundColor: Colors.blueGrey,
         body: Center(
@@ -18,26 +26,47 @@ class CurrencyConverterMaterialPage extends StatelessWidget{
                 color: Color.fromARGB(255, 255, 255, 255),
               )
               ),
-              TextField(
-                style: const TextStyle(
-                  color: Colors.black,
-                ),
-                decoration: InputDecoration(
-                  hintText: "Please enter the amount in USD",
-                  hintStyle: const TextStyle(
+              Padding(
+                padding: const EdgeInsets.all(10),
+                child: TextField(
+                  style: const TextStyle(
                     color: Colors.black,
                   ),
-                  prefixIcon: const Icon(Icons.monetization_on_outlined),
-                  prefixIconColor: Colors.black,
-                  filled:true,
-                  fillColor: Colors.white,
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: const BorderSide(
-                      width: 2.0,
-                      style: BorderStyle.solid,
+                  decoration: InputDecoration(
+                    hintText: "Please enter the amount in USD",
+                    hintStyle: const TextStyle(
+                      color: Colors.black,
                     ),
-                    borderRadius: BorderRadius.circular(40),
+                    prefixIcon: const Icon(Icons.monetization_on_outlined),
+                    prefixIconColor: Colors.black,
+                    filled:true,
+                    fillColor: Colors.white,
+                    focusedBorder: border,
+                    enabledBorder: border,
                   ),
+                  keyboardType: const TextInputType.numberWithOptions(
+                    decimal:true,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextButton(onPressed: () {
+                  
+                  // debugPrint("button clicked"); // solution 1
+                  if(kDebugMode){
+                    print("Button clicked");
+                  }
+                },
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.black,
+                  foregroundColor: Colors.white,
+                  minimumSize: const Size(double.infinity,50),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(25),
+                    ),
+                ),
+                child: const Text("Convert"),
                 ),
               ),
             ],
